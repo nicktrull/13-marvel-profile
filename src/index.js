3beeff1caa39cb5f2764b0ec7488df80cab56534
Nick Trull <nick.trull@gmail.com>
2016-03-25 21:36:59 -0500

    make sidebar get and show data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 'use strict';
+import SeriesInfoView from 'series-info-view';
+
+export default function () {
+  fetch(`http://gateway.marvel.com:80/v1/public/series/436?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    // Select profile container
+    const element = document.querySelector(`.sidebar`);
+    // New profile data
+    const profile = new SeriesInfoView(element, info.data.results[0]);
+  });
+}
