4377134e80366ff8a38295e3927b959a9921eb35
Nick Trull <nick.trull@gmail.com>
2016-03-27 01:16:40 -0500

    get modal pop ups to work

--- src/comic-view.js
+++ src/comic-view.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 export default class ComicView {
   constructor(result) {
@@ -6,6 +6,7 @@ export default class ComicView {
     this.element = document.createElement(`li`);
 
     this.renderComicView();
+    this.triggerModal();
   }
 
   renderComicView() {
@@ -16,8 +17,27 @@ export default class ComicView {
       </div>
       <div class="comic-issue"># ${this.result.issueNumber}</div>
       <div class="comic-title">${this.result.title}</div>
-      <div class="comic-description">${this.result.description}</div>
+
+      <div id="modal" class="modal--active modal">
+        <div class="modal-container">
+          <div class="modal__close">
+            <p class="modal__close--button">x</p>
+          </div>
+          <div class="modal__content">
+            <p class="modal__content">${this.result.description}</p>
+          </div>
+        </div>
+      </div>
+
       <button class="comic-description__button">Read More</button>
     `;
+  }
+
+  triggerModal() {
+    const button = this.element.querySelector(`button`);
+    const modal = this.element.querySelector(`#modal`);
+    button.addEventListener(`click`, () => {
+      modal.classList.toggle(`modal`);
+    });
   }
 }
